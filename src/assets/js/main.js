f40bdf6b7e6c45fef2c92aebe039780901f59aa6
zamir44 <tooradicalxxi@gmail.com>
2020-11-07 16:51:39 +0100

    saving project

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -6,29 +6,39 @@ import * as basicLightbox from './basicLightbox.js'
 window.addEventListener('DOMContentLoaded', (e) => {
 
     const images = document.querySelectorAll('.grid-image');
-    // const largeImages = document.querySelectorAll('.grid-half');
 
     images.forEach(image => {
         image.addEventListener('click', (e) => {
-            const imagediv = image.childNodes[5].childNodes[1];
+            const item = image.childNodes[5].childNodes[1];
 
-            basicLightbox.create(imagediv).show()
+            basicLightbox.create(item.cloneNode(true)).show()
         })
     })
 
+    const mainImages = document.querySelectorAll('.main-img');
+
+    mainImages.forEach(image => {
+        image.addEventListener('click', (e) => {
+            const item = image.childNodes[3].childNodes[3];
+
+            basicLightbox.create(item.cloneNode(true)).show()
+        })
+    })
+
+
     // sticky here
 
-    // //*** STICKY NAVIGATION ***//
-    // const header = document.getElementById("menu-wrapper");
+    //*** STICKY NAVIGATION ***//
+    const header = document.getElementById("menu-wrapper");
 
-    // window.addEventListener(
-    //   "scroll",
-    //   (e) => {
-    //     window.scrollY >= 100
-    //       ? header.classList.add("sticky")
-    //       : header.classList.remove("sticky");
-    //   },
-    //   { passive: true }
-    // );
+    window.addEventListener(
+      "scroll",
+      (e) => {
+        window.scrollY >= 68
+          ? header.classList.add("sticky")
+          : header.classList.remove("sticky");
+      },
+      { passive: true }
+    );
 
 });
