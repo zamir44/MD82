a6c0356132b0b752a0973513271783c9da9f67f2
Lokman Musliu <lokman.musliu@gmail.com>
2022-12-01 21:35:47 -0600

    updated packages and some deps

--- utils/shortcodes/picture.js
+++ utils/shortcodes/picture.js
@@ -1,54 +1,27 @@
 const Image = require("@11ty/eleventy-img");
-const sharp = require("sharp");
 
-
-module.exports = async function (src, alt, breakWidths, classes = "", style = "", outputFormat = "jpeg") {
-
+module.exports = function (src, alt, classes = "") {
   if (alt === undefined) {
     throw new Error(`Missing \`alt\` on responsiveImage from: ${src}`);
   }
 
-  const widths = Object.values(breakWidths)
-
-  let stats = await Image(src, {
-    widths: widths,
-    formats: ["webp", outputFormat],
+  let options = {
+    widths: [600, 700, 1200],
+    formats: ["webp", "jpeg"],
     urlPath: "/assets/img/",
     outputDir: "./dist/assets/img",
+  };
+
+  Image(src, options);
+
+  // get metadata even the images are not fully generated
+  let metadata = Image.statsSync(src, options);
+
+  return Image.generateHTML(metadata, {
+    class: classes,
+    alt,
+    sizes: "(min-width: 30em) 50vw, 100vw",
+    loading: "lazy",
+    decoding: "async",
   });
-
-  let lowestSrc = stats[outputFormat][0];
-
-  let lightboxImg = stats[outputFormat][2].url;
-
-  // Generate placeholder image
-  const placeholder = await sharp(lowestSrc.outputPath)
-    .resize({
-      fit: sharp.fit.inside
-    })
-    .blur()
-    .toBuffer();
-
-  // Add base64
-  const base64Placeholder = `data:image/png;base64,${placeholder.toString(
-    "base64"
-  )}`;
-
-
-  // Iterate over formats and widths
-  return `<picture>
-      ${Object.values(stats).map(imageFormat => {
-    return `<source type="image/${imageFormat[0].format}" data-srcset="${imageFormat.map(entry => `${entry.url} ${entry.width}w`).join(", ")}">`;
-  }).join("\n")}
-        <img
-          data-src="${lowestSrc.url}"
-          src="${base64Placeholder}"
-          width="${lowestSrc.width}"
-          height="${lowestSrc.height}"
-          data-light="${lightboxImg}"
-          class="lazy ${classes}"
-          style="${style}"
-          alt="${alt}">
-      </picture>`;
-
 };
